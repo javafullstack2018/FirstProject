bbfcfbf1dddf59dfac2afeb4e3be6bd247e9197f
Yang Liu <yangl@asg.com>
2018-05-03 15:08:20 -0400

    test1 skeleton

---
+++ test1.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.Arrays;
+
+public class MaxSum {
+    public static int findMaxSum(List<Integer> list) {
+        throw new UnsupportedOperationException("Waiting to be implemented.");
+    }
+    
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(5, 9, 7, 11);
+        System.out.println(findMaxSum(list));
+    }
+}

b1d7713e3057d095a7318761341286d1f95e9007
KangminLiu <liukangmin@gmail.com>
2018-05-13 10:19:07 -0400

    changed the code to do one loop instead of two, only returns the two largest number

--- test1_project/test_kangmin/src/main/java/javafullstack2018/net/App.java
+++ test1_project/test_kangmin/src/main/java/javafullstack2018/net/App.java
@@ -9,26 +9,51 @@ import java.util.Arrays;
 
 public class App {
     public static int findMaxSum(List<Integer> list) {
-        int sum = 0;
-        int sub = 0;
-        System.out.println(list.size());
+ //       int sum = 0;
+ //       int sub = 0;
+//        System.out.println(list.size());
         // sort the list in descending order using Collections Framework
 //        Collections.sort(list, Collections.reverseOrder()); 
-        // sort the list manually
-        for (int i = 0; i < list.size(); i++)
+        // sort the list manually with two loops
+//        for (int i = 0; i < list.size(); i++)
+//        {
+//        	for (int j = i + 1; j < list.size(); j++)
+//        	{
+//        		if (list.get(i) < list.get(j))
+//        		{
+//        			sub = list.get(i);
+//        			list.set(i, list.get(j));
+//        			list.set(j, sub);
+//        		}
+//        	}
+//        }
+        // just get the largest two elements with one loop
+        int first = list.get(0); // the largest element in the array list
+        int second = list.get(1); // the second largest element in the array list
+        if(list.get(0) > list.get(1))
         {
-        	for (int j = i + 1; j < list.size(); j++)
+        	first = list.get(0);
+        	second = list.get(1);
+        }
+        else
+        {
+        	first = list.get(1);
+        	second = list.get(0);
+        }
+        
+        for(int i = 2; i < list.size(); i++)
+        {
+        	if(list.get(i) > first)
         	{
-        		if (list.get(i) < list.get(j))
-        		{
-        			sub = list.get(i);
-        			list.set(i, list.get(j));
-        			list.set(j, sub);
-        		}
+        		second = first;
+        		first = list.get(i);
+        	}
+        	else if(list.get(i) > second && list.get(i) != first)
+        	{
+        		second = list.get(i);
         	}
         }
-        sum = list.get(0) + list.get(1);
-        return sum;
+        return (first + second);
     }
     
     public static void main(String[] args) {
